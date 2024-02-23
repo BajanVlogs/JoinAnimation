995a2fb929ff36be9eba2053609919ab92e1c55b
BajanVlogs <kjolly544@gmail.com>
2024-02-23 17:58:06 -0500

    Update Main.php

--- src/FireworkCelebration/Main.php
+++ src/FireworkCelebration/Main.php
@@ -5,9 +5,9 @@ namespace FireworkCelebration;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\item\Item;
-use pocketmine\world\sound\FireworkLaunchSound;
-use pocketmine\world\sound\GenericSound;
-use pocketmine\world\particle\FireworkParticle;
+use pocketmine\world\sound\LaunchSound;
+use pocketmine\world\sound\ExplodeSound;
+use pocketmine\world\particle\ExplodeParticle;
 use pocketmine\entity\Entity;
 
 class Main extends PluginBase {
@@ -27,15 +27,15 @@ class Main extends PluginBase {
         $nbt = Entity::createBaseNBT($position);
 
         // Create the entity
-        $entity = Entity::createEntity("Firework", $player->getWorld(), $nbt); // Changed $player->getLevel() to $player->getWorld()
+        $entity = Entity::createEntity("Firework", $player->getWorld(), $nbt); // Keeping $player->getWorld() as requested
 
         // Play firework launch sound
-        $player->getWorld()->addSound(new FireworkLaunchSound($player)); // Changed $player->getLevel() to $player->getWorld()
+        $player->getWorld()->addSound(new LaunchSound($player)); // Keeping $player->getWorld() as requested
 
         // Play a generic sound
-        $player->getWorld()->addSound(new GenericSound($player)); // Changed $player->getLevel() to $player->getWorld()
+        $player->getWorld()->addSound(new ExplodeSound($player)); // Keeping $player->getWorld() as requested
 
         // Spawn a firework particle
-        $player->getWorld()->addParticle(new FireworkParticle($position)); // Changed $player->getLevel() to $player->getWorld()
+        $player->getWorld()->addParticle(new ExplodeParticle($position)); // Keeping $player->getWorld() as requested
     }
 }
