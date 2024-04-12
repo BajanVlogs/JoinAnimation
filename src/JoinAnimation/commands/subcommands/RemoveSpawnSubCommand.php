4c2ba8d8e9cdae18af3117184b3bffb77fbb52e9
Lil Kev <kjolly544@gmail.com>
2024-04-12 14:28:34 -0400

    Create RemoveSpawnSubCommand.php

---
+++ src/JoinAnimation/commands/subcommands/RemoveSpawnSubCommand.php
@@ -0,0 +1,47 @@
+<?php
+ 
+namespace JoinAnimation\commands\subcommands;
+
+use pocketmine\player\Player;
+
+use pocketmine\command\CommandSender;
+
+use CortexPE\Commando\BaseSubCommand;
+
+use JoinAnimation\Loader;
+use JoinAnimation\utils\PluginUtils;
+use JoinAnimation\utils\SpawnUtils;
+
+class RemoveSpawnSubCommand extends BaseSubCommand{
+
+    public function __construct(){
+        parent::__construct("removespawn", "§r§fRemover custom JoinAnimation spawn by §bBajanVlogs", ["removejoin"]);
+        $this->setPermission("joinanimation.command.removespawn");
+    }
+    /**
+     * @return void
+     */
+    protected function prepare(): void{
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param string $aliasUsed
+     * @param array $args
+     * @return void
+     */
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+        if(!$sender instanceof Player){
+            $sender->sendMessage("Use this command in-game");
+            return;
+        }
+
+        if(!$sender->hasPermission("joinanimation.command")){
+            $sender->sendMessage(Loader::Prefix(). Loader::getMessage($sender, "Messages.no-permission"));
+            PluginUtils::PlaySound($sender, "mob.villager.no", 1, 1);
+            return;
+        }
+
+        SpawnUtils::removeSpawn($sender);
+    }
+}
